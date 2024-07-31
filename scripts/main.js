e1ff5d1bf650007b8da5b16906c904d9b1cf481f
Ashley Grenon <ashley.grenon@gmail.com>
2024-07-30 21:33:10 -0400

    Added ids for all the "leds"

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,42 @@
 const main = () => {
-   // setInterval(updateClock, 1000);
+   setInterval(updateClock, 1000);
 }
 
 const updateClock = () => {
     const currentTime = new Date(Date.now());
 
     const clock = document.getElementById("clock");
+    clock.title = currentTime.toLocaleTimeString();
 
     const hours = currentTime.getHours();
     const minutes = currentTime.getMinutes();
     const seconds = currentTime.getSeconds();
 
-    const hourSegment = document.getElementById('hour-segment');
-    hourSegment.setAttribute("number", hours);
-    const minuteSegment = document.getElementById('minute-segment');
-    minuteSegment.setAttribute("number", minutes);
-    const secondSegment = document.getElementById('second-segment');
-    secondSegment.setAttribute("number", seconds);
+    const hourDigits = getNumberAsDigitsArray(hours);
+    const minuteDigits = getNumberAsDigitsArray(minutes);
+    const secondDigits = getNumberAsDigitsArray(seconds)
 
-    console.log('hours', getNumberAsDigitsArray(hours), 'minutes', getNumberAsDigitsArray(minutes), 'seconds', getNumberAsDigitsArray(seconds));
+    updateLed("m", "tens", 4, seconds % 2);
 };
 
 const getNumberAsDigitsArray = (number) => {
     return number.toString().padStart(2, 0).split('').map(n => Number(n));
 } 
+
+const isOn = (value) => {
+
+}
+
+const updateLed = (unit, place, number, isOn) => {
+    const className = "led--on";
+    const id = `${unit}-${place}-${number}`;
+    const element = document.getElementById(id);
+
+    if (isOn) {
+        element.classList.add(className);
+    } else {
+        element.classList.remove(className);
+    }
+}
 
 main();
